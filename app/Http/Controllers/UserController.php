10b5c1a539b09283a18eb95e660e18aa19386db6
Aditya Rizqiqa Ramadhan <rizqiqaaditya@gmail.com>
2026-03-11 17:22:27 +0700

    Praktikum 2.3_Jobsheet 4_PWL

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,7 +30,11 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::where('level_id', 2)->count();
+        // dd($user); // langkah 2: aktifkan untuk melihat output, langkah 3: nonaktifkan agar tampil di view
         return view('user', ['data' => $user]);
     }
 }
