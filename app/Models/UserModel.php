4e40ac6af151263c5a8c21109296cc15e3ed35de
Aditya Rizqiqa Ramadhan <rizqiqaaditya@gmail.com>
2026-03-11 16:23:43 +0700

    Praktikum 1_jobsheet 4_PWL

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Testing\Fluent\Concerns\Has;
 
 class UserModel extends Model
 {
@@ -12,4 +11,11 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+    ];
 }
